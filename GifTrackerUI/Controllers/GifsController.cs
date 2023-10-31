5367ffa975824ce68c692368e676335168b9c118
jcepriano <jamescepriano@gmail.com>
2023-10-31 11:02:37 -0700

    exception occuring

---
+++ GifTrackerUI/Controllers/GifsController.cs
@@ -0,0 +1,21 @@
+﻿using GifTrackerUI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GifTrackerUI.Controllers
+{
+    public class GifsController : Controller
+    {
+        private readonly IGifsApiService _gifsApiService;
+
+        public GifsController(IGifsApiService gifsApiService) 
+        { 
+            _gifsApiService = gifsApiService;
+        }
+
+        public IActionResult Index()
+        {
+            var gifs = _gifsApiService.GetGifs();
+            return View(gifs);
+        }
+    }
+}
---
+++ GifTrackerUI/Interfaces/IGifsApiService.cs
@@ -0,0 +1,9 @@
+﻿using GifTrackerUI.Models;
+
+namespace GifTrackerUI.Interfaces
+{
+    public interface IGifsApiService
+    {
+        Task<List<Gif>> GetGifs();
+    }
+}
