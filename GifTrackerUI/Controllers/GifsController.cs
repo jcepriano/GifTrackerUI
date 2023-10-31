8644abd5459320c6fe7b4f01803c1c4e4abad3cb
jcepriano <jamescepriano@gmail.com>
2023-10-31 14:36:34 -0700

    Finished Updating a gif

--- GifTrackerUI/Controllers/GifsController.cs
+++ GifTrackerUI/Controllers/GifsController.cs
@@ -1,6 +1,7 @@
 ﻿using GifTrackerUI.Interfaces;
 using GifTrackerUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace GifTrackerUI.Controllers
 {
@@ -29,6 +30,29 @@ namespace GifTrackerUI.Controllers
         public async Task<IActionResult> Index(Gif gif)
         {
             var result = await _gifsApiService.CreateGif(gif);
+            if (result)
+            {
+                return Redirect("/gifs");
+            }
+            else
+            {
+                return Redirect("/gifs/create");
+            }
+        }
+
+        [Route("gifs/{id:int}")]
+        public async Task<IActionResult> Update(int id)
+        {
+            var gifs = await _gifsApiService.GetGifs();
+            var gif = gifs.FirstOrDefault(g => g.Id == id);
+            return View(gif);
+        }
+
+        [HttpPost]
+        [Route("gifs/{id:int}")]
+        public async Task<IActionResult> Update(int id, Gif gif)
+        {
+            var result = await _gifsApiService.UpdateGif(id, gif);
             if (result)
             {
                 return Redirect("/gifs");
